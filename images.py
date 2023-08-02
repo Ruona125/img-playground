8e4b4b3a34d1f5f42970469411ba1f89cac69e11
ruona <ogheneruonaagadagba4@gmail.com>
2023-08-02 15:57:01 +0100

    initial commit

---
+++ images.py
@@ -0,0 +1,6 @@
+from PIL import Image, ImageFilter
+
+img = Image.open("./images/4.1 pikachu.jpg.jpg")
+filtered_img = img.convert('L')
+crooked = filtered_img.rotate(180)
+crooked.save("grey.png", "png")
