db9c5e1fa3e46bd5c480dfba77d7020fd8cd4751
Ruona125 <ogheneruonaagadagba4@gmail.com>
2023-08-03 11:50:42 +0100

    modified some files

--- images.py
+++ images.py
@@ -3,4 +3,4 @@ from PIL import Image, ImageFilter
 img = Image.open("./images/4.1 pikachu.jpg.jpg")
 filtered_img = img.convert('L')
 crooked = filtered_img.rotate(180)
-crooked.save("grey.png", "png")
+crooked.save("yyyy.png", "png")
